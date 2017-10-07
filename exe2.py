94fefde353c6d134a7e5522bc140da74b13507a5
groundzerion <32602494+groundzerion@users.noreply.github.com>
2017-10-08 01:55:25 +0200

    Update exe2.py

--- exe2.py
+++ exe2.py
@@ -1,1 +1,10 @@
 #parzyste czy nieparzyste
+
+print('Podaj liczbe: ')
+liczba = int(input())
+reszta = liczba % 2
+
+if reszta == 0:
+  print('Podana przez ciebie liczba jest liczba parzysta')
+else:
+  print('Podana przez ciebie liczba jest liczba  nieparzysta')
