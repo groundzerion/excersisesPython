ce8e9668f0f181bfae833f70b29eaecf22d2b6d7
groundzerion <32602494+groundzerion@users.noreply.github.com>
2017-10-08 15:26:20 +0200

    Update and rename exe2.py to exe2_extras1.py

--- exe2_extras1.py
+++ exe2_extras1.py
@@ -1,10 +1,16 @@
-#parzyste czy nieparzyste
+e#parzyste czy nieparzyste
 
 print('Podaj liczbe: ')
 liczba = int(input())
 reszta = liczba % 2
+reszta4 = liczba % 4
 
 if reszta == 0:
   print('Podana przez ciebie liczba jest liczba parzysta')
 else:
   print('Podana przez ciebie liczba jest liczba  nieparzysta')
+ 
+if reszta4 ==0:
+  print('Podana przez ciebie liczba jest podzielna przez 4')
+else:
+  print('Podana przez ciebie liczba nie jest podzielna przez 4')
